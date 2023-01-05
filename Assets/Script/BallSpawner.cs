6f00084f0b14f37b6bb64f97de1e694f4c96557e
ofgonen_office <ofgonen@gmail.com>
2023-01-05 17:30:11 +0300

    Create BallSpawnner Component

---
+++ Assets/Script/BallSpawner.cs
@@ -0,0 +1,31 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+public class BallSpawner : MonoBehaviour
+{
+#region Fields
+    [ SerializeField ] BallPool ball_pool;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
