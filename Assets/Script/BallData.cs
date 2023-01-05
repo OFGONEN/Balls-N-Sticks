bee52451585c188cdda7587cb8b42186bb64f1e8
ofgonen_office <ofgonen@gmail.com>
2023-01-05 16:26:30 +0300

    BallData: Ball Mass

--- Assets/Script/BallData.cs
+++ Assets/Script/BallData.cs
@@ -9,11 +9,13 @@ using Sirenix.OdinInspector;
 [ CreateAssetMenu( fileName = "shared_ball_data_", menuName = "FF/Game/Ball Data" ) ]
 public class BallData : ScriptableObject
 {
+    [ SerializeField, LabelText( "Mass" ) ] float ball_mass;
     [ SerializeField, LabelText( "Mesh Material" ) ] Material ball_material;
     [ SerializeField, LabelText( "Physic Material" ) ] PhysicMaterial ball_material_physic;
     [ SerializeField, LabelText( "Currency Range" ) ] Vector2 ball_currency_range;
     [ SerializeField, LabelText( "Next Data" ) ] BallData ball_nextData;
 
+	public float BallMass                    => ball_mass;
 	public Material BallMaterial             => ball_material;
 	public PhysicMaterial BallPhysicMaterial => ball_material_physic;
 	public Vector2 BallCurrency              => ball_currency_range;
