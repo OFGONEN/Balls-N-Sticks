0270ac6482f638b01a361fde8afe0566dccca43b
ofgonen_office <ofgonen@gmail.com>
2023-01-05 17:41:50 +0300

    Implement BallDataLibrary

---
+++ Assets/Script/BallDataLibrary.cs
@@ -0,0 +1,11 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "library_ball_data", menuName = "FF/Game/Library/Ball Data" ) ]
+public class BallDataLibrary : SharedDataLibrary< BallDataLibrary >
+{
+}
