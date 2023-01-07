633c2b3c2d28059613dec428c02e286a9abbd0b0
ofgonen-home <ofgonen@gmail.com>
2023-01-08 02:02:28 +0300

    Implement FingerUpdate

---
+++ Assets/Script/FingerUpdate.cs
@@ -0,0 +1,45 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+[ CreateAssetMenu( fileName = "shared_finger_update", menuName = "FF/Game/Finger Update" ) ]
+public class FingerUpdate : ScriptableObject
+{
+#region Fields
+    Vector2 finger_position_start;
+    Vector2 finger_position_end;
+
+    public Vector2 Direction => ( finger_position_end - finger_position_start ).normalized;
+    public float Delta => Mathf.Abs( ( finger_position_end - finger_position_start ).magnitude );
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void OnFingerDown( LeanFinger finger )
+    {
+		finger_position_start = finger.ScreenPosition;
+		finger_position_end   = finger.ScreenPosition;
+	}
+
+    public void OnFingerUpdate( LeanFinger finger )
+    {
+		finger_position_end = finger.ScreenPosition;
+	}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
