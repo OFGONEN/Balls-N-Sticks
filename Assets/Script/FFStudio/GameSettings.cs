510edf5a0413e9f869e400b686bc9604989d3d9c
ofgonen-home <ofgonen@gmail.com>
2023-01-08 21:07:09 +0300

    GameSettings: Character rotation settings

--- Assets/Script/FFStudio/GameSettings.cs
+++ Assets/Script/FFStudio/GameSettings.cs
@@ -19,7 +19,10 @@ namespace FFStudio
 		[ LabelText( "Movement Forward Speed" ) ] public float character_movement_forward_speed;
 		[ LabelText( "Movement Lateral Speed" ) ] public float character_movement_lateral_speed;
 		[ LabelText( "Movement Lateral Clamp" ) ] public Vector2 character_movement_lateral_clamp;
+		[ LabelText( "Movement Rotate Speed" ) ] public float character_movement_rotate_speed;
 		[ LabelText( "Movement Rotate Cofactor" ) ] public float character_movement_rotate_cofactor;
+		[ LabelText( "Movement Rotate Deceleration" ) ] public float character_movement_rotate_deceleration;
+		[ LabelText( "Movement Rotate Clamp" ) ] public Vector2 character_movement_rotate_clamp;
 
     [ Title( "Camera" ) ]
         [ LabelText( "Follow Speed (Z)" ), SuffixLabel( "units/seconds" ), Min( 0 ) ] public float camera_follow_speed_depth = 2.8f;
