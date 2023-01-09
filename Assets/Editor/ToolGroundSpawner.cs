af7481a91e63b218ad0e96f4dd4b4f548d45b0d7
ofgonen-home <ofgonen@gmail.com>
2023-01-10 00:25:00 +0300

    ToolGroundSpawner: Delete environment objects

--- Assets/Editor/ToolGroundSpawner.cs
+++ Assets/Editor/ToolGroundSpawner.cs
@@ -2,12 +2,16 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using FFStudio;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEditor.SceneManagement;
 
-[ CreateAssetMenu( fileName = "tool_level_ground_spawn", menuName = "FF/Game/Tool/Ground Spawn" ) ]
+[ CreateAssetMenu( fileName = "tool_ground_spawner", menuName = "FF/Game/Tool/Ground Spawn" ) ]
 public class ToolGroundSpawner : ScriptableObject
 {
 #region Fields
+    List< GameObject > environemt_gameObject_list = new List< GameObject >();
 #endregion
 
 #region Properties
@@ -17,6 +21,31 @@ public class ToolGroundSpawner : ScriptableObject
 #endregion
 
 #region API
+    [ Button() ]
+    public void DeleteEnvironment()
+    {
+		EditorSceneManager.MarkAllScenesDirty();
+
+		var environmentStart = GameObject.Find( "--- Environment_Start ---" ).gameObject;
+		var environmentEnd   = GameObject.Find( "--- Environment_End ---" ).gameObject;
+
+		var startindex = environmentStart.transform.GetSiblingIndex();
+		var endIndex   = environmentEnd.transform.GetSiblingIndex();
+
+		var sceneObjects = EditorSceneManager.GetActiveScene().GetRootGameObjects();
+
+		for( var i = startindex + 1; i < endIndex; i++ )
+        {
+			environemt_gameObject_list.Add( sceneObjects[ i ] );
+		}
+
+        for( var i = 0; i < environemt_gameObject_list.Count; i++ )
+        {
+			GameObject.DestroyImmediate( environemt_gameObject_list[ i ] );
+		}
+
+		environemt_gameObject_list.Clear();
+	}
 #endregion
 
 #region Implementation
